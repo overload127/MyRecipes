c6d57e2bbf3fa9e73df0d7d55f89196108f0b24e
Vinogradov Konstantin <kostya.06.05.1995@gmail.com>
2023-06-24 11:13:24 +0300

    Fix gitignore rules for back. Add backend with auth jwt.

---
+++ backend/backend/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView
+)
+
+
+urlpatterns = [
+    path('inner/grappelli/', include('grappelli.urls')),
+    path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('inner/boss_admin/', admin.site.urls),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/token/logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    import debug_toolbar
+    urlpatterns.append(path('__debug__/', include(debug_toolbar.urls)))
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
